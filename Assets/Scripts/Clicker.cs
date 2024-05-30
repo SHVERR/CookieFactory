20ec172eca903b80b70b77309b76f51c0e92d2b7
Evgeniy Shadrin <shevlad82@gmail.com>
2024-05-31 02:00:34 +0300

    Добавить .gitattributes и .gitignore.

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        нажатие ЛКМ
+        добавление денег на счет
+        покупка завода клавишей B (увеличивает в 2 раза заработок)
+         
+        */
+
+        int count;
+        
+    }
+}
